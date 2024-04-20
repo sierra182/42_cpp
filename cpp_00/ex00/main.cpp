9320052daad8a021145bd55b6e31a4f0ade49d22
Sebastien Vidot <svidot@c2r7p4.42perpignan.fr>
2024-04-20 20:08:21 +0200

    wip ex

--- cpp_00/ex00/main.cpp
+++ cpp_00/ex00/main.cpp
@@ -6,12 +6,19 @@
 /*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 15:13:15 by svidot            #+#    #+#             */
-/*   Updated: 2024/04/20 15:15:05 by svidot           ###   ########.fr       */
+/*   Updated: 2024/04/20 19:03:50 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-int main()
+#include <iostream>
+
+int main(int argc, char *argv[])
 {
- ;//   printf("nique toi\n");
+    if (argc <= 1)
+        return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" 
+                << std::endl, 0);        
+    while (*++argv)
+        std::cout << *argv; 
+    std::cout << std::endl;
+    return (0);
 }
