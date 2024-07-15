5fce07c78799f3e118549c9be6ce596ca581cd7a
Sebastien Vidot <svidot@c1r2p9.42perpignan.fr>
2024-07-15 16:02:38 +0200

    wip brainz new zomb

--- cpp_01/ex00/main.cpp
+++ cpp_01/ex00/main.cpp
@@ -1,7 +1,9 @@
 #include "Zombie.hpp"
 
+Zombie* newZombie( std::string name );
 int main( void )
 {
     Zombie zombie("test");
-    
+    Zombie *zombies = newZombie("moncul");
+    delete(zombies);
 }
