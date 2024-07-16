0fd7fcb919e2afb2c10e5f6ca9e405c45ee8d0b0
sierra182 <sierra-echo@msn.com>
2024-07-16 16:24:59 +0200

    wip ex04

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 14:20:12 by seblin            #+#    #+#             */
-/*   Updated: 2024/07/16 14:39:38 by seblin           ###   ########.fr       */
+/*   Updated: 2024/07/16 16:19:13 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,31 @@
 int	main(int argc, char *argv[])
 {
 	if (argc != 4)
-		return (std::cout << "need three parameters" << std::endl, (1));
-(void) argv;
-	std::ifstream inf("infile");
+		return (std::cerr << "need three parameters" << std::endl, (1));
+	std::ifstream inf(argv[1]);
 	if (!inf)
-		return (std::cout << "Error file" << std::endl, (1));
-		
+		return (std::cerr << "Error file in" << std::endl, (1));
+	std::string s0 = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	if (s1.empty())
+		return (std::cerr << "parameter two is empty" << std::endl, (1));		
+	std::ofstream ouf((s0 + ".replace").c_str());
+	if (!ouf)
+		return (std::cerr << "Error file out" << std::endl, (1));
+	std::string line;
+	while (std::getline(inf, line))
+	{
+		std::cout << line << std::endl;
+		size_t finded;
+		if ((finded = line.find(s1)) != std::string::npos)
+		{
+			line.erase(finded, s1.length());
+			line.insert(finded, s2);
+		}
+		ouf << line << std::endl;			
+	}	
+	inf.close();
+	ouf.close();	
 	return (0);
 }
