018f12d30f0ce3ef88376fd25f42631942c36347
sierra182 <sierra-echo@msn.com>
2024-07-18 08:21:28 +0200

    add file ex02

---
+++ cpp_02/ex01/Fixed.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 12:44:43 by seblin            #+#    #+#             */
+/*   Updated: 2024/07/18 08:04:41 by seblin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_value = 0;
+	return ;
+}
+
+Fixed::Fixed( Fixed const & src )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->_value = 0;
+	*this = src;
+	return ;
+}
+
+Fixed&	Fixed::operator=( Fixed const & rhs )
+{
+	std::cout << "Copy assignment operator called" << std::endl;	
+	 if (this->getRawBits() != rhs._value)	
+		this->_value = rhs._value;	
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_value);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+}
+
---
+++ cpp_02/ex01/Fixed.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 12:44:39 by seblin            #+#    #+#             */
+/*   Updated: 2024/07/17 17:13:15 by seblin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class Fixed
+{
+	public:
+	
+		Fixed( void );
+		virtual ~Fixed( void );
+		
+		Fixed( Fixed const & src );
+		Fixed& operator=( Fixed const & rhs );
+				
+		int		getRawBits ( void ) const;
+		void	setRawBits ( int const raw );
+		
+	private:
+	
+		int					_value;
+		static const int	_bit = 8;
+		
+};
