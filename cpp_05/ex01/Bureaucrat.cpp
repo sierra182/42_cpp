373880840aa1ff03e76dcfa9829fbaf335ade77b
sierra182 <sierra-echo@msn.com>
2024-07-29 15:49:41 +0200

    start ex01

---
+++ cpp_05/ex01/Bureaucrat.cpp
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 17:34:27 by seblin            #+#    #+#             */
+/*   Updated: 2024/07/29 15:37:09 by seblin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+Bureaucrat::Bureaucrat( void )
+{	
+	return ;
+}
+Bureaucrat::~Bureaucrat( void )
+{
+	return ;
+}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException
+	( const Bureaucrat & bur ) : _bur(bur)
+{
+	return ;
+}
+
+Bureaucrat::GradeTooHighException::GradeTooHighException
+	( const Bureaucrat & bur ) : _bur(bur)
+{
+	return ;
+}
+
+void Bureaucrat::isGradeAccept( int grade ) const
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException(*this);
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException(*this);
+}
+
+const char * Bureaucrat::GradeTooLowException::what( void ) const throw()
+{
+	std::cerr << this->_bur;
+	return " \e[1mException: Grade is too low!\e[0m";
+}
+
+const char * Bureaucrat::GradeTooHighException::what( void ) const throw()
+{
+	std::cerr << this->_bur;
+	return " \e[1mException: Grade is too hight!\e[0m";
+}
+
+Bureaucrat::Bureaucrat( std::string const name, int const grade ) :
+	_name(name)
+{
+	this->_grade = grade;
+	Bureaucrat::isGradeAccept(this->_grade);
+	std::cout << *this << " Created with succes." << std::endl;
+	
+	return ;
+}
+
+Bureaucrat::Bureaucrat( const Bureaucrat & src ) : _name(src._name)
+{
+	*this = src;
+	return ;
+}
+Bureaucrat & 		Bureaucrat::operator=( const Bureaucrat & rhs )
+{
+	if (this != &rhs)		
+		if (this->_grade != rhs._grade)
+			this->_grade = rhs._grade;
+	return (*this);	
+}
+
+const std::string &	Bureaucrat::getName( void ) const
+{
+	return (this->_name);
+}
+
+int					Bureaucrat::getGrade( void ) const
+{
+	return (this->_grade);
+}
+	
+void				Bureaucrat::incrementGrade( void )
+{
+	std::cout << *this << " Try increment grade..." << std::endl;
+	Bureaucrat::isGradeAccept(this->_grade - 1);
+	this->_grade--;
+	std::cout << *this << " Incremented." << std::endl;
+}
+void				Bureaucrat::decrementGrade( void )
+{
+	std::cout << *this << " Try decrement grade..." << std::endl;
+	Bureaucrat::isGradeAccept(this->_grade + 1);	
+	this->_grade++;
+	std::cout << *this << " Decremented." << std::endl;
+}
+
+std::ostream & operator<<( std::ostream & lhs, const Bureaucrat & rhs )
+{
+	lhs << ' ' << rhs.getName() << ", bureaucrat grade " << rhs.getGrade()
+		<< '.';
+	return (lhs);
+}
---
+++ cpp_05/ex01/main.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/29 09:39:38 by seblin            #+#    #+#             */
+/*   Updated: 2024/07/29 15:24:48 by seblin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+// #include <stdexcept>
+#include <iostream>
+
+int main (void )
+{	
+	std::cout << std::endl;
+	try	
+	{
+		Bureaucrat bur("marguerite", 0);
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl;
+	try	
+	{
+		Bureaucrat bur("marguerite", 151);
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+	std::cout << std::endl;
+	try
+	{
+		Bureaucrat bur("marguerite", 1);
+		bur.decrementGrade();
+		bur.decrementGrade();
+		bur.incrementGrade();	
+		bur.incrementGrade();	
+		bur.incrementGrade();	
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	std::cout << std::endl;
+	try
+	{
+		Bureaucrat bur("marguerite", 150);
+		bur.incrementGrade();	
+		bur.incrementGrade();	
+		bur.decrementGrade();	
+		bur.decrementGrade();
+		bur.decrementGrade();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}	
+	std::cout << std::endl;
+	return (0);
+}
