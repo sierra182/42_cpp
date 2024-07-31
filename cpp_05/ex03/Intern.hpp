a4b03bae2262bb9f53122552d14d724bcace97ad
sierra182 <sierra-echo@msn.com>
2024-07-31 22:03:31 +0200

    wip night

--- cpp_05/ex03/Intern.hpp
+++ cpp_05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 20:33:53 by seblin            #+#    #+#             */
-/*   Updated: 2024/07/31 21:11:00 by seblin           ###   ########.fr       */
+/*   Updated: 2024/07/31 22:02:05 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 #include <ostream>
 #include "AForm.hpp"
 
+#define FRM 3
+
 class Intern
 {
-	AForm * makeForm( const std::string name, const std::string target ) const;
+	void (Intern::*forms[FRM]) ( const std::string ) const;
 	
 	public:
 
@@ -24,4 +26,7 @@ class Intern
 		virtual ~Intern( void );
 		Intern( Intern const & src );
 		Intern & operator=( const Intern & rhs);
+		
+		AForm * makeForm( const std::string name, const std::string target )
+			const;
 };
--- cpp_05/ex03/main.cpp
+++ cpp_05/ex03/main.cpp
@@ -6,54 +6,24 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 09:39:38 by seblin            #+#    #+#             */
-/*   Updated: 2024/07/31 20:27:28 by seblin           ###   ########.fr       */
+/*   Updated: 2024/07/31 21:52:59 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdlib>
 #include <iostream>
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "Intern.hpp"
 
 int main ( void )
 {	
 	std::cout << std::endl;
-	std::string shrubName = "christmasTree";
-	try
-	{		
-		PresidentialPardonForm pres("cypher");
-		RobotomyRequestForm rob("zorg");
-		ShrubberyCreationForm shrub(shrubName);
-		
-		Bureaucrat bur("marguerite", 73);
-		Bureaucrat bur2("marge", 46);
-	
-		bur.executeForm(rob);	
-		bur2.signForm(rob);	
-		bur2.executeForm(rob);		
-		std::cout <<std::endl;
-		bur2.incrementGrade();
-		std::cout <<std::endl;		
-		bur2.executeForm(rob);
-		bur.executeForm(pres);
-		bur.signForm(pres);
-		Bureaucrat homer_bur("homer", 1);
-		homer_bur.signForm(pres);
-		bur.executeForm(pres);
-		std::cout <<std::endl;
-		homer_bur.executeForm(pres);
-		homer_bur.executeForm(shrub);
-		bur.signForm(shrub);
-		bur.executeForm(shrub);	
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;		
-	}
-	system(("cat " +  shrubName + "_shrubbery").c_str());
+	Intern someRandomIntern;
+	AForm* rrf; //? Form *
+	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
 	std::cout <<std::endl;
 	return (0);
 }
