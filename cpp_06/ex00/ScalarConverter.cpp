e4f1d8a9b800add1ef1137a54b8dac203aa87792
Sebastien Vidot <svidot@c1r2p10.42perpignan.fr>
2024-08-03 11:42:22 +0200

    wip ex00

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 16:51:19 by seblin            #+#    #+#             */
-/*   Updated: 2024/08/03 11:12:26 by svidot           ###   ########.fr       */
+/*   Updated: 2024/08/03 11:40:17 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,44 +66,64 @@ void ScalarConverter::convert( std::string const & entry )
 				<< std::endl;		
 			return;
 		}
+		std::cout << " char : impossible" << std::endl;
+		std::cout << " int : impossible" << std::endl;
+		std::cout << " float : nanf" << std::endl;
+		std::cout << " int : nan" << std::endl;
+		return ;	
+	}
+	if (entry.find_first_of('.') != std::string::npos)
+	{		
+		ss.clear();
+		ss.seekg(0, std::ios::beg);
+		ss >> ent_double;
+		if (ss)
+		{	std::cout << "[DOUBL] :" <<std::endl;
+			std::cout << " char : '" << static_cast<char> (ent_double) << '\'' << std::endl;
+			std::cout << " int: " << static_cast<int> (ent_double) << std::endl;	
+			std::string suff = ".0";
+			if (ent_double - static_cast<int>(ent_double)) 
+				suff = "";
+			std::cout << " float: " << static_cast<float> (ent_double) << suff + 'f'
+				<< std::endl;
+			std::cout << " double: " << static_cast<double> (ent_double) << suff
+				<< std::endl;
+			return;
+		}
+		std::cout << " char : impossible" << std::endl;
+		std::cout << " int : impossible" << std::endl;
+		std::cout << " float : nanf" << std::endl;
+		std::cout << " int : nan" << std::endl;
+		return ;
+	}
+	
+	if (entry.length() == 1 && !std::isdigit(*entry.c_str()))
+	{			
+		ss.clear();
+		ss.seekg(0, std::ios::beg);
+		ss >> ent_char;
+		if (ss)
+		{	std::cout << "[CHAR] :" <<std::endl;
+		
+			if (std::isprint(ent_char))			
+				std::cout << " char : '" << static_cast<char>(ent_char) << '\'' << std::endl;
+			else
+				std::cout << " char : Non displayable" << std::endl;
 
+			std::cout << " int: " << static_cast<int> (ent_char) << std::endl;
+			std::cout << " float: " << static_cast<float> (ent_char) << ".0f" 
+				<< std::endl;
+			std::cout << " double: " << static_cast<double> (ent_char) << ".0"
+				<< std::endl;		
+			return;
+		}
+		std::cout << " char : impossible" << std::endl;
+		std::cout << " int : impossible" << std::endl;
+		std::cout << " float : nanf" << std::endl;
+		std::cout << " int : nan" << std::endl;
+		return ;
 	}
 	
-	ss.clear();
-	ss.seekg(0, std::ios::beg);
-	ss >> ent_double;
-	if (ss && entry.find_first_of('.') != std::string::npos)
-	{	std::cout << "[DOUBL] :" <<std::endl;
-		std::cout << " char : '" << static_cast<char> (ent_double) << '\'' << std::endl;
-		std::cout << " int: " << static_cast<int> (ent_double) << std::endl;	
-		std::string suff = ".0";
-		if (ent_double - static_cast<int>(ent_double)) 
-			suff = "";
-		std::cout << " float: " << static_cast<float> (ent_double) << suff + 'f'
-			<< std::endl;
-		std::cout << " double: " << static_cast<double> (ent_double) << suff
-			<< std::endl;
-		return;
-	}
-	
-	ss.clear();
-	ss.seekg(0, std::ios::beg);
-	ss >> ent_char;
-	if (ss && entry.length() == 1 && !std::isdigit(*entry.c_str()))
-	{	std::cout << "[CHAR] :" <<std::endl;
-	
-		if (std::isprint(ent_char))			
-			std::cout << " char : '" << static_cast<char>(ent_char) << '\'' << std::endl;
-		else
-			std::cout << " char : Non displayable" << std::endl;
-
-		std::cout << " int: " << static_cast<int> (ent_char) << std::endl;
-		std::cout << " float: " << static_cast<float> (ent_char) << ".0f" 
-			<< std::endl;
-		std::cout << " double: " << static_cast<double> (ent_char) << ".0"
-			<< std::endl;		
-		return;
-	}
 	ss.clear();
 	ss.seekg(0, std::ios::beg);
 	ss >> ent_int;
@@ -120,14 +140,14 @@ void ScalarConverter::convert( std::string const & entry )
 			<< std::endl;
 		return;
 	}
-	if (entry == "nan")
-	{		
+	// if (entry == "nan")
+	// {		
 		std::cout << " char: impossible" << std::endl;
 		std::cout << " int: impossible" << std::endl;
 		std::cout << " float: nanf" << std::endl;
 		std::cout << " double: nan" << std::endl;
 		return;
-	}
+	// }
 	
 
 	// 	std::cout << "fail!" << std::endl;
