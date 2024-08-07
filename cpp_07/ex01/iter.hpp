4c18e91fd17fe8c82f08717dcb7d58e2074965f5
sierra182 <sierra-echo@msn.com>
2024-08-07 12:28:13 +0200

    end cpp7 ex01

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -6,22 +6,40 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:55:55 by seblin            #+#    #+#             */
-/*   Updated: 2024/08/06 17:54:15 by seblin           ###   ########.fr       */
+/*   Updated: 2024/08/07 12:19:10 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
+#include <typeinfo>
 
-template <typename T, typename U>
-void iter(T * array, const U lenght, void (*f)(T arg))
+template <typename T>
+void iter( T * array, const size_t lenght, void (*f)(T arg) )
 {
-	for (U i = 0; i < lenght; i++)
+	for (size_t i = 0; i < lenght; i++)
 		f(array[i]);
+	std::cout << std::endl << " *********" << std::endl;
 }
 
 template <typename T>
-void myfunct(T arg)
+void myfunct( T arg )
 {
-	std::cout << arg << std::endl;
+	std::cout << "\e[1;35m you're in a generic template \e[0m" <<std::endl;
+	std::cout << "\e[1;31m " << arg  << "\e[0m" << std::endl;
+	std::cout << " my info type is: " << typeid(arg).name() << std::endl;
+	std::cout <<  " my weight is : " << sizeof(arg) << " ko" 
+		<<  std::endl;
+	std::cout << std::endl;
+}
+
+template <>
+void myfunct<std::string>( std::string arg )
+{
+	std::cout << "\e[1;35m you're in a specialized template \e[0m" <<std::endl;
+	std::cout << "\e[1;31m " << arg  << "\e[0m" << std::endl;
+	std::cout << " my info type is: " << typeid(arg).name() << std::endl;
+	std::cout <<  "\e[5m my lenght is : " << arg.length() << "\e[0m"
+		<< std::endl;
+	std::cout << std::endl;
 }
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:51:53 by seblin            #+#    #+#             */
-/*   Updated: 2024/08/06 17:21:14 by seblin           ###   ########.fr       */
+/*   Updated: 2024/08/07 12:26:04 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,32 @@
 
 int main (void )
 {
-	#define LEN 5
-	std::string arr[LEN] = {"salut", "ca", "va", "?"};
-	iter(arr, LEN, myfunct);
+	{	
+		std::cout << std::endl;
+		int arr[] = {42, 24, 21};
+		iter(arr, 3, myfunct);
+	}
+	{	
+		std::cout << std::endl;
+		char arr[] = {'z', 's', 'h'};
+		iter(arr, 3, myfunct);
+	}	
+	{	
+		std::cout << std::endl;
+		float arr[] = {42.2f, 0.1f, 974.974f};
+		iter(arr, 3, myfunct);
+	}
+	{	
+		std::cout << std::endl;
+		double arr[] = {42.2, 0.1, 974.974};
+		iter(arr, 3, myfunct);
+	}
+	{	
+		std::cout << std::endl;	
+		std::string arr[] = {"je", "souffre", "d'un", "manque", "cruel",
+			"d'imagination"};
+		iter(arr, 6, myfunct);
+	}	
+	std::cout << std::endl;
 	return (0);
 }
