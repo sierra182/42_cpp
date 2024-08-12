3e6ca4bf81dbab1cb310d0b11a8bd29614406884
Sebastien Vidot <svidot@c1r1p10.42perpignan.fr>
2024-08-12 15:38:02 +0200

    fix cpp1 ex02

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 06:27:44 by seblin            #+#    #+#             */
-/*   Updated: 2024/07/16 06:35:13 by seblin           ###   ########.fr       */
+/*   Updated: 2024/08/12 15:32:43 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main( void )
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
 	std::cout << &str << std::endl;
-	std::cout << &stringPTR << std::endl;
+	std::cout << stringPTR << std::endl;
 	std::cout << &stringREF << std::endl;
 	std::cout << str << std::endl;
 	std::cout << *stringPTR << std::endl;
