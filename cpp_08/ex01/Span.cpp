48edee74975ad7be4a8d17461e7bd51746ddd5ca
sierra182 <sierra-echo@msn.com>
2024-08-14 08:40:16 +0200

    wip cpp8 ex01

--- cpp_08/ex01/Span.cpp
+++ cpp_08/ex01/Span.cpp
@@ -6,19 +6,19 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 16:53:38 by seblin            #+#    #+#             */
-/*   Updated: 2024/08/13 18:16:35 by seblin           ###   ########.fr       */
+/*   Updated: 2024/08/14 07:39:16 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 #include <cstring>
 
-Span::Span( void ): _set(NULL)
+Span::Span( void ): _N(0), _set(NULL)
 {
 	return ;
 }
 
-Span::Span( unsigned int N ): _set(new std::set<int>)
+Span::Span( unsigned int N ): _N(N), _set(new std::set<int>)
 {
 	
 }
@@ -28,20 +28,28 @@ Span::~Span( void )
 	return ;
 }
 
-Span::Span( const Span & src )
+Span::Span( const Span & src ): _N(src._N)
 {
+	*this = src; 
 	return ;
 }
 
 Span & Span::operator=( const Span & rhs )
 {
-	return ;
+	if (this != &rhs)
+		*this->_set = *rhs._set; //! verif
+	return (*this);
 }
 
-void Span::addNumber( void )
+void Span::addNumber( int nbr )
 {
-	
+	if (this->_set && this->_set->size() < _N)
+		_set->insert(nbr);
+	else
+		throw std::overflow_error("Max is reached"); //!
 }
+
+
 
 void Span::shortestSpan( void )
 {
--- cpp_08/ex01/Span.hpp
+++ cpp_08/ex01/Span.hpp
@@ -6,26 +6,44 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 16:51:20 by seblin            #+#    #+#             */
-/*   Updated: 2024/08/13 18:00:21 by seblin           ###   ########.fr       */
+/*   Updated: 2024/08/14 08:35:55 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <set>
+#include <stdexcept>
 
 class Span
 {
 	Span( void );
-	virtual ~Span( void );
 	Span( const Span & src );
 	Span & operator=( const Span & rhs );
 
-	void addNumber( void );
+
+	
 	void shortestSpan( void );
 	void longestSpan( void );
-
+	
+	const unsigned int _N;
 	std::set<int> * _set;
 	
 	public:
+	void addNumber( int nbr );
+	template <class Iter>
+	void addNumber( Iter start, Iter end );
+	
+		virtual ~Span( void );
 		Span( unsigned int N );
 };
+
+template <class Iter>
+void Span::addNumber( Iter start, Iter end )
+{
+  //! if end <= start error
+	
+	if (this->_set && this->_set->size() + std::distance(start, end) <= _N)
+		_set->insert(start, end);
+	else
+		throw std::overflow_error("Max is reached"); //!
+}
