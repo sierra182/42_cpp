5907aab8063402fe862e544dc2ebcb86e28a5dc5
Sebastien Vidot <svidot@c2r5p8.42perpignan.fr>
2024-08-16 10:02:02 +0200

    wip test

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 06:42:01 by seblin            #+#    #+#             */
-/*   Updated: 2024/08/16 09:48:15 by svidot           ###   ########.fr       */
+/*   Updated: 2024/08/16 10:01:27 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,31 +29,54 @@ int getRandom()
 
 int main( void )
 {
-	std::cout << std::endl << std::endl;
+	std::cout << std::endl << std::endl << std::endl;
 	{
-	std::cout << "\e[1;3;90;103;9m\t ☣️  Test: exception N max ☣️  \e[0m"
-		<< std::endl << std::endl << std::endl;
+		std::cout << " ~~~~~~~~~~~~~~~";
+		std::cout << "\e[1;3;90;103;9m ☣️  Test: exception N max ☣️  \e[0m"
+			<< std::endl << std::endl << std::endl;
 	
 	try	{
 		std::cout << "\e[3m * create span with N = 0 \e[0m" << std::endl;		
 		Span sp(0);
 		std::cout << "\e[3m * add a number \e[0m" << std::endl
 			<< std::endl;
-		sp.addNumber(1);	
+		sp.addNumber(1);
 	}
 	catch(const std::exception & e)
 		{ std::cerr << "\e[1;31m\t   Exception: " << e.what() << "\e[0m"
 			<< std::endl << std::endl; }
 	
 	try	{
+		std::cout << "\e[3m * create span with N = 4 \e[0m" << std::endl;
 		Span sp(4);
+		std::cout << "\e[3m * add a plage of 5 items \e[0m" << std::endl
+			<< std::endl;
 		int arr[] = {2, 1, 99, -100, -200};
 		sp.addNumber(arr, arr + 5);	
 	}
 	catch(const std::exception & e)
 		{ std::cerr << "\e[1;31m\t   Exception: " << e.what() << "\e[0m"
+			<< std::endl << std::endl; }	
+	}
+	std::cout << std::endl << std::endl;
+	{
+		std::cout << " ~~~~~~~~~~~~~~~";
+		std::cout << "\e[1;3;90;103;9m ☣️  Test: exception inversed \
+iterator ☣️  \e[0m"	<< std::endl << std::endl << std::endl;
+		
+		try	{
+		std::cout << "\e[3m * create span with N = 4 \e[0m" << std::endl;
+		Span sp(4);
+		std::cout << "\e[3m * add an inversed plage of 5 items \e[0m" << std::endl
+			<< std::endl;
+		int arr[] = {2, 1, 99, -100, -200};
+		sp.addNumber(arr + 5, arr);	
+	}
+	catch(const std::exception & e)
+		{ std::cerr << "\e[1;31m\t   Exception: " << e.what() << "\e[0m"
 			<< std::endl << std::endl; }
 	}
+		
 	return (0);
 }
 
