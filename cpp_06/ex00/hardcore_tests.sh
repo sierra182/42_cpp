d8430bf00d49f54643907ef7607cc09d331b0a45
Sebastien Vidot <svidot@c2r6p8.42perpignan.fr>
2024-08-25 16:34:46 +0200

    wip tests

--- cpp_06/ex00/hardcore_tests.sh
+++ cpp_06/ex00/hardcore_tests.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# test=6
-# echo "testing value: " $test
-# ./conversionOfScalarTypes $test gyWE78pxhzaph782qY
-
-# test=42.0f
-# echo "testing value: " $test
-# ./conversionOfScalarTypes $test gyWE78pxhzaph782qY
-
-# test=42.0
-# echo "testing value: " $test
-# ./conversionOfScalarTypes $test gyWE78pxhzaph782qY
-
 values=(6 42.0f 42.0 "hjh")
 index=0
 while [ $index -lt ${#values[@]} ]
@@ -19,4 +7,5 @@ do
     echo -e "\n\033[1;31m testing value: " ${values[$index]} "\033[0m"
     ./conversionOfScalarTypes ${values[$index]} gyWE78pxhzaph782qY
     ((index++))
+    sleep 1
 done
--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -6,13 +6,14 @@
 /*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 11:28:49 by seblin            #+#    #+#             */
-/*   Updated: 2024/08/25 16:19:09 by svidot           ###   ########.fr       */
+/*   Updated: 2024/08/25 16:33:54 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "ScalarConverter.hpp"
 #include <cstdlib>
+#include <unistd.h>
 
 int	main( int argc, char * argv[])
 {	
@@ -30,10 +31,17 @@ int	main( int argc, char * argv[])
 	std::cout << std::endl;
 	if (!(argc == 3 && std::string(argv[1]) == "gyWE78pxhzaph782qY"))
 	{
-	std::cout << "---enter h for hardcore_tests" << std::endl;
-	if (std::cin.get() == 'h')	 
+	std::cout << "\e[0m---enter h for hardcore_tests" << std::endl;
+	if (std::cin.get() == 'h')
+	{
 		std::cout << std::endl;
-	 system("./hardcore_tests.sh");	 
+		std::cout << "\e[1;5;36m" << 
+			" 🥶 HARD_CORE TEST LAUNCHED - KEEP ARMS INSIDE THE RIDE 🥶"
+		<< "\e[0m" << std::endl;
+		sleep(4);
+	 	system("./hardcore_tests.sh");	 
+		
+	}	 
 	}
 	
 	return (0);
