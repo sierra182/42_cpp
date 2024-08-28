92cc0a5bbd245f3b7cbfda779cc8d88fd6c4b92c
Sebastien Vidot <svidot@c2r5p8.42perpignan.fr>
2024-08-28 16:18:35 +0200

    wip cpp9

--- cpp_09/ex00/main.cpp
+++ cpp_09/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:47:23 by seblin            #+#    #+#             */
-/*   Updated: 2024/08/26 19:21:37 by seblin           ###   ########.fr       */
+/*   Updated: 2024/08/28 16:17:26 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,39 @@
 #include <cerrno>
 #include <cstring>
 #include "MyStyl.hpp"
+
+#include <map>
+
+bool parseDate(std::string date, std::string::iterator & it,  int max, int delim)
+{
+	int nbr = 0;
+	
+	
+	while (it != date.end() && *it != delim)
+	{
+		while (std::isspace(*it))
+			it = date.erase(it);
+		while (std::isdigit(*it))
+			nbr++; 
+		if (nbr > max)
+			return std::cout << "too much digit" << std::endl, false;
+		if (!std::isdigit(*it))
+			return std::cout << "not digit" << std::endl, false;		
+	}
+	return true;
+}
+
+void	parseLine(std::string line)
+{
+	std::map<std::string, float> mymap;
+	std::string::iterator it = line.begin();
+	if (parseDate(line, it, 4, '-') && parseDate(line, it, 2, '-') && parseDate(line, it, 2, '|'))
+	{
+		
+		// line.substr(0, std::distance(line.begin(), it));
+	}	
+	
+}
 
 int main(int argc, char * argv[])
 {
@@ -29,7 +62,7 @@ int main(int argc, char * argv[])
 	while (std::getline(inf, line))
 	{
 		std::cout << line << std::endl;
-		line.find()
+		// line.find()
 		
 	}	
 		
