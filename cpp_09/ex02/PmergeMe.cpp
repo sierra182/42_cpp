1a369d51d2da8c9bac5968c45b10652c62267835
sierra182 <sierra-echo@msn.com>
2024-09-06 14:35:13 +0200

    del space end file

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
+/*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 17:04:38 by svidot            #+#    #+#             */
-/*   Updated: 2024/09/05 15:52:55 by svidot           ###   ########.fr       */
+/*   Updated: 2024/09/06 14:34:09 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,4 +59,3 @@ PmergeMe::PmergeMe(char *argv[])
 		<< " elements with std::deque : " << rDeq.first << " us" << std::endl;
 	std::cout << std::endl;
 }
-
